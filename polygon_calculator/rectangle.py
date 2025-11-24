557cc6f0f0aa054a4a58a2c4d5593976df0bd5a7
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-24 06:03:26 +0530

    Create rectangle.py

---
+++ polygon_calculator/rectangle.py
@@ -0,0 +1,32 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        return "\n".join("*" * self.width for _ in range(self.height)) + "\n"
+
+    def get_amount_inside(self, shape):
+        if shape.width > self.width or shape.height > self.height:
+            return 0
+        return (self.width // shape.width) * (self.height // shape.height)
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
